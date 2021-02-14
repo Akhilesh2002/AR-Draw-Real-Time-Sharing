146098b9b0f9c78ba109aa4c0eadd7be03a13db8
Akhilesh2002 <akhileshmehra80@gmail.com>
2021-02-14 21:23:51 +0530

    Updated FIles

---
+++ Assets/Project/Project02/Script/objectSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class objectSpawn : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    GameObject spawnningObject;
+   // public GameObject cameraRef;
+    public void spawn()/*GameObject spawningObject)*/
+    {
+        photonView.RPC("Spawning", RpcTarget.All /*, spawningObject*/);   
+    }
+
+    [PunRPC]
+    void Spawning()/* GameObject spawnningObject)*/
+    {
+        Instantiate(spawnningObject, gameObject.transform.position + new Vector3(0, 0, 3f), Quaternion.identity);
+    }
+
+}
